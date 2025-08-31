ec9bf141bf557a91e37cdbf3fd47c837b9b8e406
basavesha <basavesha@thorsignia.in>
2025-08-31 19:53:45 +0530

    Add PostgreSQL support: configure database for production deployment

--- backend/travel_blog/settings.py
+++ backend/travel_blog/settings.py
@@ -80,12 +80,20 @@ WSGI_APPLICATION = 'travel_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+import dj_database_url
+
+# Use PostgreSQL in production, SQLite in development
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
